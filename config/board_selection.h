9ec23448abf46dfc50111b6a7ec0769f56b509e8
mcxufly <mcxufly@outlook.com>
2024-08-11 19:59:14 +0800

    add makerdiary/nrf52840_usb_dongle.h config for correct pin ocnnection

--- config/board_selection.h
+++ config/board_selection.h
@@ -8,7 +8,9 @@
 
 //--------------------------------------------------------------------+
 // MakerDiary M.2 DOCK
-#define OPT_BOARD_MAKERDIARY_M2_DOCK    1
+#define OPT_BOARD_MAKERDIARY_M2_DOCK    1           
+// MakerDiary NRF52840 USB DONGLE
+#define OPT_BOARD_MAKERDIARY_NRF52840_USB_DONGLE    3
 
 // nRF52840 Dongle (aka PCA10059)
 #define OPT_BOARD_NRF52840_DONGLE       2
@@ -20,6 +22,8 @@
     #include "boards/nordic/pca10059.h"
 #elif CFG_BOARD == OPT_BOARD_MAKERDIARY_M2_DOCK
     #include "boards/makerdiary/m2_dock.h"
+#elif CFG_BOARD == OPT_BOARD_MAKERDIARY_NRF52840_USB_DONGLE
+    #include "boards/makerdiary/nrf52840_usb_dongle.h"
 #else
     #error "No board selected, please select your board in CMakeLists.txt"
 #endif
